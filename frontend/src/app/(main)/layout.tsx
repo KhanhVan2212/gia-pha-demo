574def8ea52e567905bcb2185313d7fefdfee71f
Khanhvan2212 <nguyenkhanhvan221202@gmail.com>
2026-03-12 11:37:51 +0700

    feat: Remove ProtectedRoute Layout

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import { Sidebar } from "@/components/layout/sidebar";
 import { Header } from "@/components/layout/header";
-import { ProtectedRoute } from "@/components/protected-route";
 
 export default function MainLayout({
   children,
@@ -8,16 +7,14 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ProtectedRoute>
-      <div className="flex min-h-screen">
-        <Sidebar />
-        <div className="flex flex-1 min-w-0 flex-col">
-          <Header />
-          <main className="flex-1 min-w-0 overflow-hidden p-4 lg:p-6">
-            {children}
-          </main>
-        </div>
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex flex-1 min-w-0 flex-col">
+        <Header />
+        <main className="flex-1 min-w-0 overflow-hidden p-4 lg:p-6">
+          {children}
+        </main>
       </div>
-    </ProtectedRoute>
+    </div>
   );
 }
